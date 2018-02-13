554a2e6df186a517962b0e24d79b6f469fc97d80
Shikher Verma <root@shikherverma.com>
2018-02-13 19:26:37 +0530

    Add Hot swapping of theme

--- app/src/main/java/com/shikherverma/themingarchitecture/SettingsActivity.kt
+++ app/src/main/java/com/shikherverma/themingarchitecture/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.shikherverma.themingarchitecture
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v4.app.NavUtils
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.support.v7.preference.PreferenceManager
@@ -10,19 +11,23 @@ import android.support.v7.preference.SwitchPreferenceCompat
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        if (sharedPreferences.getBoolean("pref_key_theme", false)) setTheme(R.style.AppThemeNight)
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment()).commit()
     }
+
+    override fun onBackPressed() = NavUtils.navigateUpFromSameTask(this)
 
     class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
         override fun onCreatePreferences(bundle: Bundle?, s: String?) {
             addPreferencesFromResource(R.xml.pref_settings)
             val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-            onSharedPreferenceChanged(sharedPreferences, "pref_key_theme")
-            onSharedPreferenceChanged(sharedPreferences, "pref_key_name")
-            onSharedPreferenceChanged(sharedPreferences, "pref_key_email")
+            setSummary(sharedPreferences, "pref_key_theme")
+            setSummary(sharedPreferences, "pref_key_name")
+            setSummary(sharedPreferences, "pref_key_email")
         }
 
         override fun onResume() {
@@ -31,9 +36,16 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+            setSummary(sharedPreferences, key)
+            if (key == "pref_key_theme") {
+                activity.recreate()
+            }
+        }
+
+        private fun setSummary(sharedPreferences: SharedPreferences, key: String) {
             val preference = findPreference(key)
             if (preference is SwitchPreferenceCompat) {
-                if(sharedPreferences.getBoolean(key, false))
+                if (sharedPreferences.getBoolean(key, false))
                     preference.setSummary("Enabled")
                 else
                     preference.setSummary("Disabled")
